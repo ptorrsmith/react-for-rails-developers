b9e4953e2ad00661e9a22f21ad13c2f5e533464d
Peter Torr Smith <peter.torr.smith@cultureamp.com>
2021-10-11 18:23:05 +1300

    Set root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'pages/index' # this will get index.html.erb from templates
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pages#index' # this will enact the pages controller index action
 end
